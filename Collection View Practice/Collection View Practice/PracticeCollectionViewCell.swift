63202095e5b15d05a02c58002c6e1295cc6a8e21
Aidan Frye <51840505+Fr3nchFry3@users.noreply.github.com>
2024-05-09 15:39:41 -0400

    Add files via upload

---
+++ Collection View Practice/Collection View Practice/PracticeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PracticeCollectionViewCell.swift
+//  Collection View Practice
+//
+//  Created by lab on 3/19/24.
+//
+
+import UIKit
+
+class PracticeCollectionViewCell: UICollectionViewCell {
+    //add practiceLabel
+    
+    
+    @IBOutlet weak var practiceLabel: UILabel!
+}
